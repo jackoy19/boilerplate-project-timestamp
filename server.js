1dd4f1800c2453d3860f901b5293ca46ee1ed046
jackoy19 <95595573+jackoy19@users.noreply.github.com>
2022-01-05 02:11:43 +0000

    timestamp

--- server.js
+++ server.js
@@ -25,20 +25,12 @@ app.get("/api/hello", function (req, res) {
 });
 
 
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-
-app.get("/api/timestamp/:date_string", (req, res) => {
+app.get("/api/:date_string", (req, res) => {
   let dateString = req.params.date_string;
 
-  //A 4 digit number is a valid ISO-8601 for the beginning of that year
-  //5 digits or more must be a unix time, until we reach a year 10,000 problem
   if (/\d{5,}/.test(dateString)) {
     let dateInt = parseInt(dateString);
-    //Date regards numbers as unix timestamps, strings are processed differently
+    
     res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
   } else {
     let dateObject = new Date(dateString);
@@ -49,4 +41,43 @@ app.get("/api/timestamp/:date_string", (req, res) => {
       res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
     }
   }
+});
+
+app.get("/api/timestamp", function(req, res) {
+  // We'll save the current timestamp as a Date object...
+  let newDate = new Date();
+  // ... then we'll redirect to the correct endpoint, adding the date to the end of the URL:
+  // A 302 HTTP status code, also known as "Found", is used when the Web page is temporarily not available for reasons that have not been unforeseen. That way, search engines don't update their links.
+  // NB: If we don't specify a redirect type, express.js defaults to "302 Found". For clarity, we add the 302 here:
+  res.redirect(302, "/api/timestamp/" + newDate.getFullYear() + "-" + (newDate.getUTCMonth() + 1) + "-" + newDate.getUTCDate() );    // NB: Date object months are zero-indexed, so we add 1 to the month value of our date query.
+});
+
+
+
+
+app.get("/api/timestamp/:dateReq", function(req, res) {
+
+  let date;
+
+  if ( /\D/.test(req.params.dateReq) ) {
+    date = new Date( req.params.dateReq );
+  }
+  else {
+    date = new Date( parseInt(req.params.dateReq) );
+  };
+  let utcDate = date.toUTCString();
+  let unixDate = date.getTime();
+  
+  res.json({
+    "unix": unixDate,
+    "utc": utcDate
+  });
+  
+});
+  
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
